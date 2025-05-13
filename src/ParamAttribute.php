eaa729698a1e2d258ddf37efb3840bb9a00b722a
Andrew Masiye <amasiye313@gmail.com>
2025-05-13 10:03:00 +0200

    feat: create ParamAttribute.php

---
+++ src/ParamAttribute.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Assegai\Attributes;
+
+/**
+ * Base class for all parameter attributes.
+ *
+ * @package Assegai\Attributes
+ */
+abstract class ParamAttribute
+{
+  public readonly mixed $value;
+}
